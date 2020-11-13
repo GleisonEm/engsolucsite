d2f392f5ba74b783dbbb6d5fd5b374ca1160f372
gleison1208 <gleison.spfc1208@hotmail.com>
2020-11-13 17:44:30 -0300

    ruled heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://67.207.92.246/api/v1'
+    baseURL: '//67.207.92.246/api/v1'
 });
 console.log(api);
 export default api;
